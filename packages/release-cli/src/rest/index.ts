7f8ac4289890d318d25030a12ca9beb0b149d5ec
KirillBaranov <kirillBaranovJob@yandex.ru>
2025-11-30 21:43:33 +0300

    refactor(release): update REST index
    - Update REST index

--- packages/release-cli/src/rest/index.ts
+++ packages/release-cli/src/rest/index.ts
@@ -1,4 +1,4 @@
 // REST handlers exports for release plugin
-export * from './handlers/plan-handler.js';
-export * from './handlers/report-handler.js';
+export * from './handlers/plan-handler';
+export * from './handlers/report-handler';
 
